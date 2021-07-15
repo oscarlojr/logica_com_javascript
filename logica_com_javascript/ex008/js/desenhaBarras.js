0d46b28906ee479f491fa36898fbdbda57e48a26
Oscar Lima Oliveira Junior <oscarlojr@gmail.com>
2021-07-15 06:30:25 -0300

    Desenha barras funcionand

--- logica_com_javascript/ex008/js/desenhaBarras.js
+++ logica_com_javascript/ex008/js/desenhaBarras.js
@@ -1,16 +1,22 @@
 
-function desenhaBarras(x,y,cores){
+function desenhaBarras(x,y,serie2015,cores){
     var tela = document.querySelector('canvas');
-    var pincel = tela.getContext('2d')
-    
+    var pincel = tela.getContext('2d');   
 
     pincel.fillStyle = cores;
-    pincel.fillRect(0,x,0,y)
+    pincel.fillRect(x,y,50,serie2015);
 
 }
-desenhaBarras(50,50,'blue');
-desenhaBarras(50,25,'green');
-// //var cores = ['blue', 'green', 'yellow', 'red'];
-// //var serie2015 = [50, 25, 20, 5];
-// //var serie2016 = [65, 20, 13, 2];
+
+desenhaBarras(0,0,50,'blue');
+desenhaBarras(0,50,25,'green');
+desenhaBarras(0,75,20,'yellow');
+desenhaBarras(0,95,5,'red');
+
+
+desenhaBarras(100,0,65,'blue');
+desenhaBarras(100,65,20,'green');
+desenhaBarras(100,85,13,'yellow');
+desenhaBarras(100,98,2,'red');
+
 
